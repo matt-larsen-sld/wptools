5cb090e9bd250778390c24d750c6d0b87e98e9bc
Matt Larsen <matt.larsen@connorgp.com>
2019-08-22 15:10:59 -0600

    README install and usage

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wptools",
+    name="wordpresstools",
     version="0.0.1",
     author="Matthew Larsen",
     author_email="matt.larsen@connorgp.com",
